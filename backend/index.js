36a71ff3b7abe01dc98762257f10480e5e3c1085
ST-Ko <sithu.ko21120@gmail.com>
2024-07-27 15:14:33 +1000

    fso part 3

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
-const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -18,9 +22,57 @@ let notes = [
   },
 ];
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(notes));
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+
+  return maxId;
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  res.json(note);
 });
 
 const PORT = 3001;
---
+++ part3/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let data = [
+  {
+    id: "1",
+    name: "Arto Hella",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(data);
+});
+
+app.get("/api/info", (req, res) => {
+  const date = new Date();
+  res.send(`<p>Phonebook has info for ${data.length} people.<br/><br/>${date}`);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server is running on port: ${PORT}`);
