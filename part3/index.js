e74e6e90cfc9cf2a78b13e5d4b2e10d5ccba47e5
ST-Ko <sithu.ko21120@gmail.com>
2024-07-27 15:43:55 +1000

    assignment 3.1-3.6

--- part3/index.js
+++ part3/index.js
@@ -39,6 +39,56 @@ app.get("/api/info", (req, res) => {
   res.send(`<p>Phonebook has info for ${data.length} people.<br/><br/>${date}`);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  const person = data.find((data) => data.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  data = data.filter((data) => data.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  // Check for missing name or number
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  // Check for duplicate names
+  const nameExists = data.some((data) => data.name === body.name);
+
+  if (nameExists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const newId = Math.floor(Math.random() * 10000000);
+
+  const newPerson = {
+    id: newId,
+    name: body.name,
+    number: body.number,
+  };
+
+  data = data.concat(newPerson);
+
+  res.json(newPerson);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server is running on port: ${PORT}`);
