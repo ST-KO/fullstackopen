b45bcd84cbbd9501e1b61e16c0d83df5aa31036c
ST-Ko <sithu.ko21120@gmail.com>
2024-07-31 15:53:42 +1000

    assignment 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.env.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://scytherock74:${password}@cluster0.gfwaidq.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+// bcFcLd5lrlVIq4kZ
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const People = mongoose.model("People", peopleSchema);
+
+if (process.argv.length > 3) {
+  const people = new People({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  people.save().then((result) => {
+    console.log(
+      `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+    );
+    mongoose.connection.close();
+  });
+} else {
+  People.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((people) => {
+      console.log(`${people.name} ${people.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
