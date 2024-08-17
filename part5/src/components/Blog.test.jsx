a9ea3a36a61e31b11c46bc5242f5ad5cc5e4a283
ST-Ko <sithu.ko21120@gmail.com>
2024-08-17 10:55:32 +1000

    assignment 5.15

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 import { expect } from 'chai';
 
-test('renders content', () => {
+describe('<Blog />', () => {
     const blog = {
         title: 'Component testing is done with react-testing-library',
         author: 'siko',
@@ -12,40 +12,48 @@ test('renders content', () => {
         user: {
             id: 'aaa'
         }
-    }
+    };
+    
+    test('renders content', () => {
+        render(<Blog blog={blog} />);
+        const title = screen.getByText(blog.title, { exact: false });
+        const author = screen.getByText(blog.author, { exact: false });
+        expect(title).toBeDefined();
+        expect(author).toBeDefined();
+    
+        const url = screen.queryByText(blog.url);
+        const likes = screen.queryByText(blog.likes);
+        expect(url).toBeNull();
+        expect(likes).toBeNull();
+    });
+    
+    test('shows the URL and number of likes when the button is clicked', async () => {
+        const { container } = render(<Blog blog={blog} />);
+    
+        const user = userEvent.setup();
+        const button = screen.getByText('view');
+        await user.click(button);
+    
+        const url = container.querySelector('.blog-url');
+        const likes = container.querySelector('.blog-likes');
+    
+        expect(url).toHaveTextContent(blog.url);
+        expect(likes).toHaveTextContent(`likes ${blog.likes}`);
+    });
 
-    render(<Blog blog={blog} />);
-    const title = screen.getByText(blog.title, { exact: false });
-    const author = screen.getByText(blog.author, { exact: false });
-    expect(title).toBeDefined();
-    expect(author).toBeDefined();
+    test('clicking the likes button twice calls event handler twice', async () => {
+        const mockHandler = vi.fn();
 
-    const url = screen.queryByText(blog.url);
-    const likes = screen.queryByText(blog.likes);
-    expect(url).toBeNull();
-    expect(likes).toBeNull();
+        render(<Blog blog={blog} updateLikes={mockHandler} />);
+
+        const user = userEvent.setup();
+        const viewButton = screen.getByText('view');
+        await user.click(viewButton);
+
+        const likesButton = screen.getByText('likes');
+        await user.click(likesButton);
+        await user.click(likesButton);
+
+        expect(mockHandler.mock.calls).toHaveLength(2);
+    })
 });
-
-test('shows the URL and number of likes when the button is clicked', async () => {
-    const blog = {
-        title: 'Component testing is done with react-testing-library',
-        author: 'siko',
-        url: 'http://adb',
-        likes: 10,
-        user: {
-            id: 'aaa'
-        }
-    }
-
-    const { container } = render(<Blog blog={blog} />);
-
-    const user = userEvent.setup();
-    const button = screen.getByText('view');
-    await user.click(button);
-
-    const url = container.querySelector('.blog-url');
-    const likes = container.querySelector('.blog-likes');
-
-    expect(url).toHaveTextContent(blog.url);
-    expect(likes).toHaveTextContent(`likes ${blog.likes}`);
-})
