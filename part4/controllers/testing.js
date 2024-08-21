52ab0762cdadb7f38f44b9526f28efcc6cd96215
ST-Ko <sithu.ko21120@gmail.com>
2024-08-21 17:55:43 +1000

    assignment 5.18

---
+++ part4/controllers/testing.js
@@ -0,0 +1,12 @@
+const testingRouter = require("express").Router();
+const Blog = require("../model/blog.js");
+const User = require("../model/user.js");
+
+testingRouter.post("/reset", async (req, res) => {
+  // await Blog.deleteMany({});
+  await User.deleteMany({});
+
+  res.status(200).end();
+});
+
+module.exports = testingRouter;
--- part5/e2ecypress/cypress/e2e/blog_app.cy.js
+++ part5/e2ecypress/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,12 @@
 describe("Blog app", () => {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "Si Ko",
+      username: "siko",
+      password: "123456",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3003/");
   });
 
@@ -8,5 +15,28 @@ describe("Blog app", () => {
     cy.contains("Log in to application");
     cy.contains("username");
     cy.contains("password");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("siko");
+      cy.get("#password").type("123456");
+      cy.get("#login-button").click();
+
+      cy.contains("siko logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("siko");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
+
+      // cy.get(".error").contains("Wrong credentials");
+      cy.get(".error")
+        .should("contain", "Wrong credentials")
+        .and("have.css", "color", "rgb(255, 0, 0)");
+    });
   });
 });
